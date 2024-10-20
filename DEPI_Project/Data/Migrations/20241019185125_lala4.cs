ae2f6008ea920b88cf213d93d1db9a4041f7ef81
claraayman27@gmail.com <claraayman27@gmail.com>
2024-10-20 19:13:55 +0300

    fourth

---
+++ DEPI_Project/Data/Migrations/20241019185125_lala4.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DEPI_Project.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class lala4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Products_ProductId",
+                table: "Comment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Users_UserId",
+                table: "Comment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment");
+
+            migrationBuilder.RenameTable(
+                name: "Comment",
+                newName: "comments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_UserId",
+                table: "comments",
+                newName: "IX_comments_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_ProductId",
+                table: "comments",
+                newName: "IX_comments_ProductId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_comments",
+                table: "comments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_comments_Products_ProductId",
+                table: "comments",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_comments_Users_UserId",
+                table: "comments",
+                column: "UserId",
+                principalSchema: "security",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_comments_Products_ProductId",
+                table: "comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_comments_Users_UserId",
+                table: "comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_comments",
+                table: "comments");
+
+            migrationBuilder.RenameTable(
+                name: "comments",
+                newName: "Comment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_comments_UserId",
+                table: "Comment",
+                newName: "IX_Comment_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_comments_ProductId",
+                table: "Comment",
+                newName: "IX_Comment_ProductId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Products_ProductId",
+                table: "Comment",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Users_UserId",
+                table: "Comment",
+                column: "UserId",
+                principalSchema: "security",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+    }
+}
---
+++ DEPI_Project/Data/Migrations/20241019215350_lala5.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DEPI_Project.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class lala5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTokens_Users_UserId",
+                schema: "security",
+                table: "UserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserTokens",
+                schema: "security",
+                table: "UserTokens");
+
+            migrationBuilder.RenameTable(
+                name: "UserTokens",
+                schema: "security",
+                newName: "Product");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Product",
+                table: "Product",
+                columns: new[] { "UserId", "LoginProvider", "Name" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Users_UserId",
+                table: "Product",
+                column: "UserId",
+                principalSchema: "security",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Users_UserId",
+                table: "Product");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Product",
+                table: "Product");
+
+            migrationBuilder.RenameTable(
+                name: "Product",
+                newName: "UserTokens",
+                newSchema: "security");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserTokens",
+                schema: "security",
+                table: "UserTokens",
+                columns: new[] { "UserId", "LoginProvider", "Name" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTokens_Users_UserId",
+                schema: "security",
+                table: "UserTokens",
+                column: "UserId",
+                principalSchema: "security",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+    }
+}
